6f73b095aae4f6447aa79b4d1cfeb2291e15cf6f
miguel2512-UTF <miguelangelwilchez1001@gmail.com>
2022-09-25 00:22:11 -0500

    Ejercicio 6: Ajustes

--- assets/js/ejercicio6.js
+++ assets/js/ejercicio6.js
@@ -2,14 +2,14 @@ const form=document.getElementById('form-usuario')
 const campos=document.querySelectorAll('#form-usuario input')
 
 const reglas={
-    numeros: /^[0-9]{5,}/,
-    nombre: /^[a-zA-Z0-9]/,
-    apellido: /^[a-zA-Z0-9]/,
-    vacio: /^[a-z]{1,}/
+    numeros: /^[0-9]{10,10}$/,
+    nombre: /^[A-Za-zñÀ-ÿ ]{1,20}$/,
+    apellido: /^[A-Za-zñÀ-ÿ ]{1,20}$/,
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    password: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/
 }
 
 const inputs={
-    tipodoc: false,
     documento: false,
     nombre: false,
     apellido: false,
@@ -22,7 +22,7 @@ form.addEventListener("submit",e =>{
 
     const terminos=document.getElementById('terminos')
 
-    if (inputs.tipodoc && inputs.documento && inputs.nombre && inputs.apellido && inputs.correo && inputs.password && terminos.checked) {
+    if (inputs.documento && inputs.nombre && inputs.apellido && inputs.correo && inputs.password && terminos.checked) {
         alert("El usuario ha sido registrado correctamente")
         form.reset()
         document.querySelectorAll('.success').forEach((icono)=>{
@@ -35,9 +35,6 @@ form.addEventListener("submit",e =>{
 
 const validarCampos=(e)=>{
     switch (e.target.name) {
-        case "tipodoc":
-            validarInput(reglas.vacio,e.target,e.target.name)
-        break;
         case "documento":
             validarInput(reglas.numeros,e.target,e.target.name)
         break;
@@ -46,6 +43,15 @@ const validarCampos=(e)=>{
         break;
         case "apellido":
             validarInput(reglas.apellido,e.target,e.target.name)
+        break;
+        case "correo":
+            validarInput(reglas.correo,e.target,e.target.name)
+        break;
+        case "password":
+            validarInput(reglas.password,e.target,e.target.name)
+        break;
+        case "password2":
+            validarPassword()
         break;
     }
 }
@@ -59,25 +65,29 @@ const validarInput=(regla,input,grupo)=>{
     if (regla.test(input.value)) {
         document.querySelector(`.g-${grupo}`).classList.remove('error')
         document.querySelector(`.g-${grupo}`).classList.add('success')
+        document.querySelector(`#grupo__${grupo} .error__message`).classList.remove('activo')
         inputs[grupo]=true
     }else{
         document.querySelector(`.g-${grupo}`).classList.add('error')
         document.querySelector(`.g-${grupo}`).classList.remove('success')
+        document.querySelector(`#grupo__${grupo} .error__message`).classList.add('activo')
         inputs[grupo]=false
     }
 }
 
 const validarPassword=()=>{
-    const pass1=document.querySelector('.password')
-    const pass2=document.querySelector('.password2')
+    const pass1=document.querySelector('.g-password')
+    const pass2=document.querySelector('.g-password2')
 
     if (pass1.value===pass2.value) {
-        document.querySelector(`.g-${grupo}`).classList.remove('error')
-        document.querySelector(`.g-${grupo}`).classList.add('success')
+        document.querySelector(`.g-password2`).classList.remove('error')
+        document.querySelector(`.g-password2`).classList.add('success')
+        document.querySelector(`#grupo__password2 .error__message`).classList.remove('activo')
         inputs['password']=true
     }else{
-        document.querySelector(`.g-${grupo}`).classList.add('error')
-        document.querySelector(`.g-${grupo}`).classList.remove('success')
+        document.querySelector(`.g-password2`).classList.add('error')
+        document.querySelector(`.g-password2`).classList.remove('success')
+        document.querySelector(`#grupo__password2 .error__message`).classList.add('activo')
         inputs['password']=false
     }
 }
